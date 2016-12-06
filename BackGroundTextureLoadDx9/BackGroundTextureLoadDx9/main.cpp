6ec37d5a98964e45083782ccc2127f1a08cd8750
暇厨 <fortis1019@yahoo.co.jp>
2016-12-06 16:20:52 +0900

    オンメモリとの時間比較

--- BackGroundTextureLoadDx9/BackGroundTextureLoadDx9/main.cpp
+++ BackGroundTextureLoadDx9/BackGroundTextureLoadDx9/main.cpp
@@ -1,15 +1,121 @@
 #include <windows.h>
 #include "window.h"
 #include "Graphics.h"
-
+#include "Macro.h"
+#include<chrono>
+#include<vector>
+#include<iostream>
+#include<fstream>
 
 #pragma comment(lib,"winmm.lib")
 #pragma comment(lib,"d3d9.lib")
 #pragma comment(lib,"d3dx9.lib")
 
+std::chrono::milliseconds LoadTexture(Graphics& graphics)
+{
+	IDirect3DTexture9*	pTexture[10];
+	auto pDevice = graphics.Device;
+	std::string path("tex1.jpg");
+
+	D3DXIMAGE_INFO imageInfo;
+	D3DXGetImageInfoFromFile(path.c_str(), &imageInfo);
+
+	std::chrono::milliseconds total = std::chrono::milliseconds::zero();
+
+	for (int i = 0; i < 10; i++)
+	{
+		auto start = std::chrono::system_clock::now();
+		D3DXCreateTextureFromFileEx
+		(
+			pDevice,
+			path.c_str(),
+			imageInfo.Width,
+			imageInfo.Height,
+			1,
+			0,
+			D3DFMT_A8R8G8B8,
+			D3DPOOL_DEFAULT,
+			D3DX_DEFAULT,
+			D3DX_DEFAULT,
+			0,
+			&imageInfo,
+			NULL,
+			&pTexture[i]
+		);
+		total +=
+			std::chrono::duration_cast<std::chrono::milliseconds>(
+				std::chrono::system_clock::now() - start);
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		SAFE_RELEASE(pTexture[i]);
+	}
+
+	return total / 10;
+}
+std::chrono::milliseconds LoadTextureInMemory(Graphics& graphics)
+{
+	auto pDevice = graphics.Device;
+	std::string path("tex1.jpg");
+
+	std::vector<char> data;
+	std::fstream stream;
+	stream.open(path, std::ios::binary | std::ios::in);
+
+	stream.seekg(0, std::ios::end);
+	auto tell = stream.tellg();
+	stream.clear();
+	stream.seekg(0, std::ios::beg);
+	auto head = stream.tellg();
+
+	auto size = tell - head;
+	data.resize(size);
+	stream.read(data.data(), size);
+	stream.close();
+
+	D3DXIMAGE_INFO imageInfo2;
+	D3DXGetImageInfoFromFileInMemory(data.data(), data.size(), &imageInfo2);
+
+	IDirect3DTexture9*	pTexture[10];
+	std::chrono::milliseconds total = std::chrono::milliseconds::zero();
+
+	for (int i = 0; i < 10; i++)
+	{
+		auto start2 = std::chrono::system_clock::now();
+		D3DXCreateTextureFromFileInMemoryEx
+		(
+			pDevice,
+			data.data(),
+			data.size(),
+			imageInfo2.Width,
+			imageInfo2.Height,
+			1,
+			0,
+			D3DFMT_A8R8G8B8,
+			D3DPOOL_DEFAULT,
+			D3DX_DEFAULT,
+			D3DX_DEFAULT,
+			0,
+			&imageInfo2,
+			NULL,
+			&pTexture[i]
+		);
+		total +=
+			std::chrono::duration_cast<std::chrono::milliseconds>(
+				std::chrono::system_clock::now() - start2);
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		SAFE_RELEASE(pTexture[i]);
+	}
+
+	return total / 10;
+}
+
 int APIENTRY _tWinMain(
-	HINSTANCE _hInstance, 
-	HINSTANCE _hPrevInstance, 
+	HINSTANCE _hInstance,
+	HINSTANCE _hPrevInstance,
 	LPTSTR _CmdLine,
 	int _CmdShow)
 {
@@ -20,10 +126,13 @@ int APIENTRY _tWinMain(
 
 	Graphics graphics;
 	graphics.Create(window.GetHwnd(), 500, 500, true);
+	auto pDevice = graphics.Device;
+
+	auto t1 = LoadTexture(graphics);
+	auto t2 = LoadTextureInMemory(graphics);
 
 	while (window.MessageProc())
 	{
-		auto pDevice = graphics.Device;
 
 		pDevice->Clear
 		(0, NULL,
