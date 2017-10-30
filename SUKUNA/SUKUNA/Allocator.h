e955a2c35588cec6ec7df2f6523f146305cd11b7
Himatyu <fortis.dedicatus@gmail.com>
2017-10-31 00:45:36 +0900

    Add blueprint

---
+++ SUKUNA/SUKUNA/Allocator.h
@@ -0,0 +1,142 @@
+/**
+* @file		Alloctor.h
+* @brief
+* @author
+* @date
+*/
+#pragma once
+
+#pragma region include
+//-----------------------------------------------------------
+// include
+//-----------------------------------------------------------
+
+#pragma endregion
+
+#pragma region define
+//-----------------------------------------------------------
+// define
+//-----------------------------------------------------------
+
+#pragma endregion
+
+namespace SUKUNA
+{
+	///実メモリポインタを持つ
+	class Mediator
+	{
+	public:
+		Mediator() {}
+		~Mediator() {}
+
+		Mediator* next;
+	private:
+
+	};
+
+	///一時的なメモリ確保を行う
+	class IFront
+	{
+	public:
+		IFront() {}
+		~IFront() {}
+
+		template<class T>
+		MemoryPointer<T> Allocate() { return MemoryPointer<T>(); }
+
+		template<class T>
+		void Deallocate(MemoryPointer<T>) {}
+
+		Mediator* GetMediators() { return nullptr; }
+		void FlipBuffer() {}
+
+	private:
+
+	};
+
+	///時間のかかるメモリ確保を行い、メディエータのポインタを差し替える
+	class IBack
+	{
+	public:
+		IBack() {}
+		~IBack() {}
+
+		void ReAllocate(Mediator*) {}
+
+		template<class T>
+		void Deallocate(MemoryPointer<T>) {}
+	private:
+
+	};
+
+	///公開用のポインタ
+	template<class T>
+	class MemoryPointer
+	{
+	public:
+		MemoryPointer() {}
+		~MemoryPointer() {}
+
+	private:
+
+	};
+
+
+	class Alloctor
+	{
+	public:
+#pragma region  defaults
+		//-----------------------------------------------------------
+		// defaults
+		//-----------------------------------------------------------
+		Alloctor() = default;
+		~Alloctor() = default;
+
+		Alloctor(const Alloctor&) = default;
+		Alloctor(Alloctor&&) = default;
+
+		Alloctor& operator=(const Alloctor&) = default;
+		Alloctor& operator=(Alloctor&&) = default;
+#pragma endregion
+
+#pragma region function
+		//-----------------------------------------------------------
+		// function
+		//-----------------------------------------------------------
+
+		template<class T>
+		MemoryPointer<T> Allocate() {}
+
+		template<class T>
+		void Deallocate(MemoryPointer<T>) {}
+
+		void PreparationForSend() {}
+
+		void SendToBack() {}
+
+		void AsyncPreparationForSend() {}
+
+		void AsyncSendToBack() {}
+
+
+#pragma endregion
+
+#pragma region property
+		//-----------------------------------------------------------
+		// property
+		//-----------------------------------------------------------
+
+#pragma endregion
+
+#pragma region value
+	//-----------------------------------------------------------
+	// val
+	//-----------------------------------------------------------
+
+#pragma endregion
+	};
+}
+
+//-----------------------------------------------------------
+// EOF
+//-----------------------------------------------------------
